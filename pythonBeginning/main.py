fc91bc1b2820372682e938457ea550d2804d51e8
Alexandr Stepuk <a.stepuk@mail.ru>
2024-04-01 10:08:16 +0300

    ДЗ_Урок 6. Семинар. Словари, множества и профилирование

--- pythonBeginning/main.py
+++ pythonBeginning/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def f(n):
+    if n == 0 or n == 1:
+        return 1
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return f(n - 1) + f(n - 2)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+n = int(input())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f(n - 2))
