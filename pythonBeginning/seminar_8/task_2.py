71debf423e51ae12e7633383ae051397480e65de
Alexandr Stepuk <a.stepuk@mail.ru>
2024-04-10 16:48:46 +0300

    ДЗ_Урок 14. Семинар. Работа с табличными данными

---
+++ pythonBeginning/seminar_8/task_2.py
@@ -0,0 +1,13 @@
+# Дан файл california_housing_train.csv.
+# Найти максимальное значение переменной "households" в зоне минимального значения переменной min_population
+# и сохраните это значение в переменную max_households_in_min_population.
+# Используйте модуль pandas.
+
+import pandas as pd
+
+df = pd.read_csv("sample_data/california_housing_train.csv")
+
+min_population = df['population'].min()
+max_households_in_min_population = df[(df['population'] == min_population)]['households'].max()
+
+print(max_households_in_min_population)
